7cc5708ff3bb56dce45f2d336652790a97b1499f
anilkumarti <anil.tiwari1403@gmail.com>
2024-05-20 11:46:06 +0530

    async await used

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,42 +1,39 @@
-import React from "react";
-import { Table, Button, Container, Navbar } from "react-bootstrap";
+import React, { Fragment, useState } from 'react'
 
 const Home = () => {
-  const seminar = [
-    { Date: "16 July", Location: "Delhi", Name: "Rahul Sinha" },
-    { Date: "19 July", Location: "Shimla", Name: "Shamsher Sahu" },
-    { Date: "30 July", Location: "Mumbai", Name: "Mohammad" },
-  ];
-  return (
-    <div> <Navbar bg="warning" variant="light">
-       <Button variant="outline-primary" className="mx-auto"> Ticket for Latest online seminar</Button>
-       </Navbar>
-      
-      <h1> Seminars</h1>
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Location</th>
-          <th> Trader </th>
-        </tr>
-      </thead>
-      <tbody>
-        {seminar.map((item, index) => (
-          <tr key={index}>
-            <td> {item.Date} </td>
-            <td> {item.Location}</td>
-            <td>{item.Name}</td>
-            <td>
-              {" "}
-              <Button> Buy tickets </Button>
-            </td>
-          </tr>
-        ))}{" "}
-      </tbody>
-    </Table>
-    </div>
-  );
-};
+  const [movies, setMovies]=useState([])
+ async function fetchMovieHandler() {
+ try {
+    const res=  await fetch('https://swapi.dev/api/films/');
+     const data=await res.json();
+  const transformedData=data.results.map((movie)=> {
+   return  {
+     id: movie.episode_id,
+    open: movie.opening_crawl
 
-export default Home;
+   };
+  
+ });
+ setMovies(transformedData)
+ }  
+ catch(error) { // Wrapped catch block in curly braces
+  console.log("there is an error -", error);
+}
+
+ }
+  return (
+    <Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}> Fetch Movies</button>
+      </section>
+      <section>
+        <ul> 
+        { movies.map(movie=> <li>{movie.title}  {movie.id} {movie.open} </li>)}
+        {console.log(movies)}
+        </ul>
+      </section>
+    </Fragment>
+  )
+}
+
+export default Home
